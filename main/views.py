53d75d464acc95cf0a931a538ef68d091f7d0cf7
Magomed_D <dzhamalovm05@gmail.com>
2025-11-13 01:53:01 +0300

    Add view of curretn notes on main page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
+from django.utils import timezone
 from django.shortcuts import render
+from .models import Note
+import datetime
 
+
+today = timezone.now().date()
 
 def home(request):
-    return render(request, "main/index.html", context=None)
+    current_notes = Note.objects.filter(dateStart__lte=today, dateEnd__gte=today)
+    data = {
+        'current_notes': current_notes,
+    }
+    return render(request, "main/index.html", context=data)
